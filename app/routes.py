1946aa07146df700f1845ae3a7a8fc570df78c5d
notfreephilly <not.philyoder93@gmail.com>
2024-04-15 20:42:01 -0700

    Created flask project and added an aboutme route

---
+++ app/routes.py
@@ -0,0 +1,14 @@
+from flask import Flask; # from the flask module imort the Flask class
+
+app = Flask(__name__) # create an instance (object) of the Flask class
+
+@app.get('/aboutme') # Flask decorator that maps view functions to routes
+def index(): # view function
+    me = {   # python dictionary     
+        "first_name": "philip",
+        "last_name": "yoder",
+        "hobbies": "binge eating",
+        "is_online": True
+    }
+    return me  # when you return a dictionary from a view function, it becomes JSON
+
