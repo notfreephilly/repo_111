10a9f1f36ab056eaad6dcfb2dad17778f55d725d
notfreephilly <not.philyoder93@gmail.com>
2024-04-17 20:03:05 -0700

    Created sandbox database and did a database challenge

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import Flask; # from the flask module imort the Flask class
 
 app = Flask(__name__) # create an instance (object) of the Flask class
 
+@app.get('/')
 @app.get('/aboutme') # Flask decorator that maps view functions to routes
 def index(): # view function
     me = {   # python dictionary     
